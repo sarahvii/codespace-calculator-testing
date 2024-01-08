8fff9e062cb5efed279ab0fceaf5bb0b3515ed2e
aagb1884 <a.a.g.blair@gmail.com>
2024-01-08 16:45:34 +0000

    do not merge yet
    tests still need work, do not merge yet

--- cypress/e2e/caclulator.cy.js
+++ cypress/e2e/caclulator.cy.js
@@ -1,5 +1,25 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://127.0.0.1:5500/index.html')
   })
+})
+
+it('can add two integers together', ()=>{
+  cy.visit('http://127.0.0.1:5500/index.html')
+  cy.get('#1').click();
+  cy.get('#add').click();
+  cy.get('#2').click();
+  cy.get('#equals').click();
+  cy.contains('#display', 3)
+ 
+})
+
+it('can multiply two integers together', ()=>{
+  cy.visit('http://127.0.0.1:5500/index.html')
+  cy.get('#2').click();
+  cy.get('#multiply').click();
+  cy.get('#4').click();
+  cy.get('#equals').click();
+  cy.contains('#display', 8)
+ 
 })
